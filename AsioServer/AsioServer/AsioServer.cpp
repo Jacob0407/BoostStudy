b74d58fa8b3860692d8131d6e3aebb469464d842
wangjiawei <it_wjw@163.com>
2020-07-14 19:48:00 +0800

    task 00: 初始化工程

---
+++ AsioServer/AsioServer/AsioServer.cpp
@@ -0,0 +1,15 @@
+﻿// AsioServer.cpp: 定义应用程序的入口点。
+//
+
+#include "AsioServer.h"
+#include "boost/thread.hpp"
+
+using namespace std;
+
+
+int main()
+{
+	
+
+	return 0;
+}
---
+++ AsioServer/AsioServer/AsioServer.h
@@ -0,0 +1,9 @@
+﻿// AsioServer.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+#include "boost/thread.hpp"
+
+// TODO: 在此处引用程序需要的其他标头。
